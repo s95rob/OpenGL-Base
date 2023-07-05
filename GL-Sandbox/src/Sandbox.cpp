26902015c4f82e3ce2e9b71fa6e08470580d5ced
rob-sh <robertjshick@gmail.com>
2023-07-04 20:07:35 -0700

    add sandbox app

---
+++ GL-Sandbox/src/Sandbox.cpp
@@ -0,0 +1,38 @@
+#include <GLBase.h>
+
+class SandboxApp : public Application {
+public:
+	virtual void Run() override;
+};
+
+int main() {
+	ApplicationInit appInit;
+	appInit.windowTitle = "GL Sandbox";
+	appInit.windowWidth = 1280;
+	appInit.windowHeight = 720;
+
+	SandboxApp* app = new SandboxApp(appInit);
+
+	app->Run();
+
+	delete app;
+
+	return 0;
+}
+
+void SandboxApp::Run() {
+	// Initialize buffers/etc. here
+
+	glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+	while (this->IsRunning()) {
+		m_window.PollEvents();
+
+		// Update here
+
+		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+		// Draw here
+
+		m_window.SwapBuffers();
+	}
+}
